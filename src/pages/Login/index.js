88812026a974307c6aef7c4864357c1a674452fd
Náder W. S. Sírio <nadersirio@gmail.com>
2023-06-22 16:49:34 -0300

    Add first files Context API's

---
+++ src/pages/Login/index.js
@@ -0,0 +1,40 @@
+import { Button } from '@material-ui/core';
+import { Container, Titulo, InputContainer } from './styles';
+import { Input, InputLabel, InputAdornment } from '@material-ui/core';
+
+export const Login = () => {
+  return (
+    <Container>
+      <Titulo>
+        Insira o seu nome
+      </Titulo>
+      <InputContainer>
+        <InputLabel>
+          Nome
+        </InputLabel>
+        <Input
+          type="text"
+        />
+      </InputContainer>
+      <InputContainer>
+        <InputLabel>
+          Saldo
+        </InputLabel>
+        <Input
+        type="number"
+        startAdornment={
+          <InputAdornment position="start">
+            R$
+          </InputAdornment>
+        }
+      />
+      </InputContainer>
+      <Button
+        variant="contained"
+        color="primary"
+      >
+        Avançar
+      </Button>
+    </Container>
+  )
+};
