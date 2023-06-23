fff0ce4f23eb763d5808c5a92602c37af221fab7
Náder W. S. Sírio <nadersirio@gmail.com>
2023-06-23 19:12:14 -0300

    add: context and provider to /feira page

--- src/pages/Feira/index.js
+++ src/pages/Feira/index.js
@@ -2,15 +2,18 @@ import { Container, Header, Lista } from './styles';
 import feira from './feira.json';
 import Produto from 'components/Produto';
 import { NavBar } from './NavBar';
+import { UsuarioContext } from 'common/context/Usuario';
+import { useContext } from 'react';
 
 export const Feira = () => {
+  const { nome, setNome, saldo, setSaldo } = useContext(UsuarioContext);
   return (
     <Container>
       <NavBar />
       <Header>
         <div>
-          <h2> Olá!</h2>
-          <h3> Saldo: R$</h3>
+          <h2> Olá! {nome}</h2>
+          <h3> Saldo: R${saldo}</h3>
         </div>
         <p>Encontre os melhores produtos orgânicos!</p>
       </Header>
